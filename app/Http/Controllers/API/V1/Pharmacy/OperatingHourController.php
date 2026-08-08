b0a866213f39ecb71108dcaee2e323d625ced381
walidSalTI <walidalsalti652@gmail.com>
2026-08-08 14:47:14 +0300

    action test

--- app/Http/Controllers/API/V1/Pharmacy/OperatingHourController.php
+++ app/Http/Controllers/API/V1/Pharmacy/OperatingHourController.php
@@ -128,6 +128,7 @@ class OperatingHourController extends Controller
             ->orderBy('day_of_week')
             ->get();
 
+
         $status = $isClosed ? 'closed for vacation' : 'reopened';
 
         return response()->json([
